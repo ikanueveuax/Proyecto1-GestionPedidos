b5f3dfbcbb1a2205403d00f622fa47623d1695e1
ikanueveuax <ikarakul@myuax.com>
2024-12-15 18:57:23 +0100

    Creacion de proyecto

---
+++ src/main/java/com/holamundo/proyecto1gestionpedidos/Proyecto1GestionPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.holamundo.proyecto1gestionpedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto1GestionPedidosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Proyecto1GestionPedidosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/holamundo/proyecto1gestionpedidos/Proyecto1GestionPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holamundo.proyecto1gestionpedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto1GestionPedidosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
